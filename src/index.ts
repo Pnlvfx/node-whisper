7a778b2e5c8ab45988ff549297241ec8e4830819
simone <simonegauli@gmail.com>
2023-09-07 02:52:46 +0200

    improvements

--- src/index.ts
+++ src/index.ts
@@ -5,69 +5,61 @@ import { AudioToTextFiles, Proto } from './types/output.js';
 import { promises as fs } from 'node:fs';
 //READ TODO ON THE OUTPUT.TS FILE
 
-type OutputTypeMap = {
-  txt: Proto;
-  vtt: Proto;
-  srt: Proto;
-  tsv: Proto;
-  json: Proto;
-};
+type WhisperOptions<T extends OutputFormat> = AudioToTextOptions & { output_format?: T };
 
-const whisper = (audio: string, options?: AudioToTextOptions) => {
-  return new Promise<AudioToTextOptions['output_format'] extends 'all' ? AudioToTextFiles : { [K in AudioToTextOptions['output_format']]: Proto }>(
-    (resolve, reject) => {
-      const params = getParams(options);
-      params.unshift(audio);
+const whisper = <T extends OutputFormat>(audio: string, options?: WhisperOptions<T>) => {
+  return new Promise<T extends undefined ? AudioToTextFiles : T extends 'all' ? AudioToTextFiles : { [K in T]: Proto }>((resolve, reject) => {
+    const params = getParams(options);
+    params.unshift(audio);
+    if (options?.verbose) {
+      console.log('command:', params);
+    }
+    const whisper = spawn('whisper', params);
+    whisper.on('error', (error) => {
+      reject(error);
+    });
+
+    whisper.stdout.on('data', (data) => {
       if (options?.verbose) {
-        console.log('command:', params);
+        console.log('stdout:', data.toString());
       }
-      const whisper = spawn('whisper', params);
-      whisper.on('error', (error) => {
-        reject(error);
-      });
+    });
 
-      whisper.stdout.on('data', (data) => {
-        if (options?.verbose) {
-          console.log('stdout:', data.toString());
-        }
-      });
+    let error = '';
 
-      let error = '';
+    whisper.stderr.on('data', (data) => {
+      if (options?.verbose) {
+        console.log('stderr:', data.toString());
+      }
+      error += data;
+    });
 
-      whisper.stderr.on('data', (data) => {
-        if (options?.verbose) {
-          console.log('stderr:', data.toString());
-        }
-        error += data;
-      });
-
-      whisper.on('close', async (code) => {
-        if (code === 1) return reject(error.toString());
-        const folder = options?.output_dir || '.';
-        const name = path.basename(audio).replace(path.extname(audio), '');
-        const outFormat = options?.output_format || 'all';
-        if (outFormat === 'all') {
-          const json = `${folder}/${name}.json`;
-          const tsv = `${folder}/${name}.tsv`;
-          const srt = `${folder}/${name}.srt`;
-          const vtt = `${folder}/${name}.vtt`;
-          const txt = `${folder}/${name}.txt`;
-          resolve({
-            json: getProto('json', json),
-            tsv: getProto('tsv', tsv),
-            srt: getProto('srt', srt),
-            vtt: getProto('vtt', vtt),
-            txt: getProto('txt', txt),
-          });
-        } else {
-          const custom = `${folder}/${name}/${outFormat}`;
-          resolve({
-            [outFormat]: custom,
-          });
-        }
-      });
-    },
-  );
+    whisper.on('close', async (code) => {
+      if (code === 1) return reject(error.toString());
+      const folder = options?.output_dir || '.';
+      const name = path.basename(audio).replace(path.extname(audio), '');
+      const outFormat = options?.output_format || 'all';
+      if (outFormat === 'all') {
+        const json = `${folder}/${name}.json`;
+        const tsv = `${folder}/${name}.tsv`;
+        const srt = `${folder}/${name}.srt`;
+        const vtt = `${folder}/${name}.vtt`;
+        const txt = `${folder}/${name}.txt`;
+        resolve({
+          json: getProto('json', json),
+          tsv: getProto('tsv', tsv),
+          srt: getProto('srt', srt),
+          vtt: getProto('vtt', vtt),
+          txt: getProto('txt', txt),
+        });
+      } else {
+        const custom = `${folder}/${name}/${outFormat}`;
+        resolve({
+          [outFormat]: custom,
+        });
+      }
+    });
+  });
 };
 
 const getProto = (key: OutputFormat, value: string): Proto => {
@@ -101,5 +93,5 @@ const getParams = (options?: AudioToTextOptions) => {
 export default whisper;
 
 const audio = path.join('media', 'audio.mp3');
-const data = await whisper(audio, { output_format: 'srt' });
+const data = await whisper(audio, { output_format: 'json' });
 console.log(await data.json.getContent());
