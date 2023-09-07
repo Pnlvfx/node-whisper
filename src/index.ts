a78419ef778bd97681bbcaaa4a13d3363a807694
simone <simonegauli@gmail.com>
2023-09-07 12:21:13 +0200

    try this

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,13 @@
 /* eslint-disable no-unused-vars */
 import { spawn } from 'node:child_process';
-import { AudioToTextOptions, OutputFormat } from './types/options.js';
+import { OutputFormat } from './types/options.js';
 import path from 'node:path';
 import { AudioToTextFiles, Proto } from './types/output.js';
 import { getProto } from './lib/proto.js';
 import { getParams } from './lib/params.js';
+import { WhisperOptions } from './lib/overload.js';
 
-export type WhisperOptions<T extends OutputFormat | undefined> = AudioToTextOptions & { output_format?: T };
-
-// Function overload for when there are options, but the output_format
-export function whisper<T extends undefined>(audio: string, options: WhisperOptions<T>): Promise<AudioToTextFiles>;
-
-// Function overload for when output_format is provided
-export function whisper<T extends OutputFormat>(
-  audio: string,
-  options: WhisperOptions<T>,
-): Promise<T extends 'all' ? AudioToTextFiles : { [K in T]: Proto }>;
-
-// Function overload for when options are not provided
-export function whisper(audio: string): Promise<AudioToTextFiles>;
-
-export function whisper<T extends OutputFormat>(audio: string, options?: WhisperOptions<T>): Promise<AudioToTextFiles | { [K in T]: Proto }> {
+function whisper<T extends OutputFormat>(audio: string, options?: WhisperOptions<T>): Promise<AudioToTextFiles | { [K in T]: Proto }> {
   return new Promise((resolve, reject) => {
     const params = getParams(options);
     params.unshift(audio);
@@ -74,7 +61,9 @@ export function whisper<T extends OutputFormat>(audio: string, options?: Whisper
   });
 }
 
+export default whisper;
+
 // //TEST
-// const audio = path.join('media', 'audio.mp3');
-// const data = await whisper(audio, { fp16: true });
-// console.log(await data.srt.getContent());
+const audio = path.join('media', 'audio.mp3');
+const data = await whisper(audio, { fp16: true });
+console.log(await data.srt.getContent());
