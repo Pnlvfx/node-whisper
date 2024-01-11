c573c5222c734603ad5d496d67d111f3f4b8fc63
simone <simonegauli@gmail.com>
2024-01-11 20:49:32 +0100

    bump deps and version

--- __tests__/whisper.test.ts
+++ __tests__/whisper.test.ts
@@ -6,8 +6,8 @@ const getFile = (file: string) => path.join(process.cwd(), 'media', file);
 describe('whisper function', () => {
   it(
     'should transcribe audio successfully',
-    async () => {
-      await whisper(getFile('audio.mp3'), { model: 'base', output_dir: path.join(process.cwd(), 'media') });
+    () => {
+      whisper(getFile('audio.mp3'), { model: 'base', output_dir: path.join(process.cwd(), 'media') });
     },
     2 * 60 * 1000,
   );
