6e60778b48abe3a9f847f6744c3a2722fffb2f47
simone <simonegauli@gmail.com>
2024-01-11 20:50:24 +0100

    revert: test

--- __tests__/whisper.test.ts
+++ __tests__/whisper.test.ts
@@ -6,8 +6,8 @@ const getFile = (file: string) => path.join(process.cwd(), 'media', file);
 describe('whisper function', () => {
   it(
     'should transcribe audio successfully',
-    () => {
-      whisper(getFile('audio.mp3'), { model: 'base', output_dir: path.join(process.cwd(), 'media') });
+    async () => {
+      await whisper(getFile('audio.mp3'), { model: 'base', output_dir: path.join(process.cwd(), 'media') });
     },
     2 * 60 * 1000,
   );
